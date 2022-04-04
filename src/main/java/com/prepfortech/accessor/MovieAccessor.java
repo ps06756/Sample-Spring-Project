b03149fa1b9d3bab85b23c7bfb6325f1e443725f
Pratik Singhal <ps06756@gmail.com>
2022-04-04 14:23:34 +0530

    Using datasource to connect to db

--- src/main/java/com/prepfortech/accessor/MovieAccessor.java
+++ src/main/java/com/prepfortech/accessor/MovieAccessor.java
@@ -1,13 +1,36 @@
 package com.prepfortech.accessor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class MovieAccessor {
+
+    @Autowired
+    private DataSource dataSource;
+
     public List<String> getMovies() {
-        return Arrays.asList("Dark night rises", "Batman begins");
+
+        List<String> movies = new ArrayList<>();
+        try(Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT title from movie";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                movies.add(rs.getString(1));
+            }
+        }
+        catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+        return movies;
     }
 }
